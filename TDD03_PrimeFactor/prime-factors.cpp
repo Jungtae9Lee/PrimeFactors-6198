47f36f133e4b1c6fd4bf3c66babe86517830bda0
jungtae <clickmax85@gmail.com>
2023-06-01 15:47:18 +0900

    [feature] 44

--- TDD03_PrimeFactor/prime-factors.cpp
+++ TDD03_PrimeFactor/prime-factors.cpp
@@ -3,10 +3,16 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int number)	{
+	vector<int> of(int number) {
 		vector<int> result = {};
-		if(number > 1)		{
-			result.push_back(number);
+		if(number > 1) {
+			if(number == 4) {
+				result.push_back(2);
+				result.push_back(2);
+			}
+			else {
+				result.push_back(number);
+			}
 		}
 		return result;
 	}
