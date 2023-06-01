17695b9f4b407ec6e986f9be12502607b6917c2b
jungtae <clickmax85@gmail.com>
2023-06-01 15:34:43 +0900

    Revert "[feature] 34"
    This reverts commit df30ec3dd218414127b53775919df7a5a9dd08a3.

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,9 +1,7 @@
 #include "pch.h"
 #include "../TDD03_PrimeFactor/prime-factors.cpp"
-using namespace std;
 
-TEST(PrimeFactors, Of1) {
-	PrimeFactor prime_factor;
-	vector<int> expected = {};
-	EXPECT_EQ(expected, prime_factor.of(1));
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
 }
--- TDD03_PrimeFactor/prime-factors.cpp
+++ TDD03_PrimeFactor/prime-factors.cpp
@@ -1,9 +1,4 @@
-#include <vector>
-using namespace std;
-class PrimeFactor{
-public:
-	vector<int> of(int i)	{
-		vector<int> result = {};
-		return result;
-	}
+class PrimeFactor
+{
+	
 };
